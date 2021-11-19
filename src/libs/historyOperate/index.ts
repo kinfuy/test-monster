489d59efb3fe9d8e383a75f94daf0ea1cd421d9f
Thezero <yonghu520@outlook.com>
2021-11-19 20:05:32 +0800

    feat: 优化细节

--- src/libs/historyOperate/index.ts
+++ src/libs/historyOperate/index.ts
@@ -1,4 +1,16 @@
-import { IOperateSet, IOperateRecord } from './types';
+import { OperateStatus, IOperateRecord, IEventType } from './types';
 import { createRandomCode } from './../utils/util';
-class OperateRecord {}
+class OperateRecord {
+  id: string = '';
+  eventType: IEventType = 'CLICK';
+  xPath: string = '';
+  formValue?: any = '';
+  status: OperateStatus = 'WAIT';
+  constructor() {}
+  run(callBack: Function) {
+    if (this.status === 'WAIT') {
+      callBack();
+    }
+  }
+}
 export default OperateRecord;
--- src/libs/historyOperate/types/index.ts
+++ src/libs/historyOperate/types/index.ts
@@ -1,19 +1,13 @@
-export type IConditions = () => Promise<boolean>;
-export interface IOperate {
-  // 点击元素
-  clickElement?: () => void;
-  // 触发事件
-  dispatchEvent?: () => void;
-}
 export type IEventType = 'CLICK' | 'FORM_INPUT';
+export type OperateStatus = 'SUCCESS' | 'FAIL' | 'WAIT';
 // 操作记录
 export interface IOperateRecord {
   id: string;
-  preconditions?: IConditions;
-  xPath: string;
   eventType: IEventType;
+  xPath: string;
   formValue?: any;
-  opearte: IOperate;
+  status: OperateStatus;
+  run: (callBack: Function) => void | Error;
 }
 
 export type IOperateSet = Map<string, IOperateRecord>;
